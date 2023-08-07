622f5df37303d9b49cb9709f9b9e94ce64196d30
zhoriefzainulmuttaqin <zhoriefzainulmuttaqin@gmail.com>
2023-08-07 06:28:21 -0400

    improvement beranda

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -2,58 +2,126 @@
 
 
 @section('content')
-<link rel="stylesheet" href="css/beranda.css">
+    <link rel="stylesheet" href="css/beranda.css">
 
-<div class="konten-atas">
-    <table>
-        <thead>
-            <tr>
-                <th class="atas-kiri">
-                    <img src="image/menhan.png" alt="">
-                </th>
-                <th>
-                    <div class="jajar">
-                        <h1 class="no-skew">Untuk Indonesia yang Lebih Baik</h1>
-                        <p class="no-skew2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iusto soluta, fugit, ad et corporis cumque alias beatae perspiciatis asperiores blanditiis. Suscipit architecto est a magnam esse, deserunt nihil tempora?</p>
-                        <button class="dukungan">Dukungan</button>
-                        <button class="relawan">Relawan</button>
-                    </div>
-                </th>
-            </tr>
-        </thead>
-    </table>
-</div>
-<div class="Ingfo" style="width: 100%; height: 625px; position: relative">
-    <div class="Rectangle9" style="width: 100%; height: 625px; left: 0px; top: 0px; position: absolute; background: #111111"></div>
-    <div class="Rectangle10" style="width: 262px; height: 69px; left: 63px; top: 53px; position: absolute; border-radius: 40px; border: 1.50px #FFCC00 solid"></div>
-    <div class="SekilasInfo" style="left: 103px; top: 73px; position: absolute; color: white; font-size: 25px;  font-weight: 800; word-wrap: break-word">SEKILAS INFO</div>
-    <img class="Rectangle11" style="width: 346px; height: 203px; left: 0px; top: 174px; position: absolute" src="https://via.placeholder.com/346x203" />
-    <img class="Rectangle12" style="width: 347px; height: 202px; left: 346px; top: 377px; position: absolute" src="https://via.placeholder.com/347x202" />
-    <img class="Rectangle13" style="width: 346px; height: 203px; left: 693px; top: 174px; position: absolute" src="https://via.placeholder.com/346x203" />
-    <div class="Rectangle14" style="width: 312px; height: 405px; left: 1039px; top: 174px; position: absolute; background: #FFCC00"></div>
-    <div class="Rectangle15" style="width: 90px; height: 40px; left: 18px; top: 188px; position: absolute; background: black"></div>
-    <div class="Berita" style="left: 36px; top: 199px; position: absolute; color: white; font-size: 15px;  font-weight: 700; word-wrap: break-word">BERITA</div>
-    <div class="Rectangle16" style="width: 161px; height: 40px; left: 361px; top: 188px; position: absolute; background: black; border: 1.50px #FFCC00 solid"></div>
-    <div class="Juli2023" style="left: 390px; top: 199px; position: absolute; color: white; font-size: 15px;  font-weight: 700; word-wrap: break-word">25 JULI 2023</div>
-    <div class="LoremIpsumDolorSitAmetConsecteturAdipiscingElitSuspendisseAliquamMolestieCongueSuspendisseVulputateNibhAcEleifendSemper" style="width: 266px; left: 361px; top: 248px; position: absolute; color: white; font-size: 15px;  font-weight: 500; word-wrap: break-word">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam molestie congue. Suspendisse vulputate nibh ac eleifend semper.</div>
-    <div class="Rectangle17" style="width: 161px; height: 40px; left: 19px; top: 403px; position: absolute; background: black; border: 1.50px #FFCC00 solid"></div>
-    <div class="Juli2023" style="left: 48px; top: 414px; position: absolute; color: white; font-size: 15px;  font-weight: 700; word-wrap: break-word">25 JULI 2023</div>
-    <div class="LoremIpsumDolorSitAmetConsecteturAdipiscingElitSuspendisseAliquamMolestieCongueSuspendisseVulputateNibhAcEleifendSemper" style="width: 266px; left: 19px; top: 463px; position: absolute; color: white; font-size: 15px;  font-weight: 500; word-wrap: break-word">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam molestie congue. Suspendisse vulputate nibh ac eleifend semper.</div>
-    <div class="Rectangle18" style="width: 161px; height: 40px; left: 720px; top: 403px; position: absolute; background: black; border: 1.50px #FFCC00 solid"></div>
-    <div class="Juli2023" style="left: 749px; top: 414px; position: absolute; color: white; font-size: 15px;  font-weight: 700; word-wrap: break-word">25 JULI 2023</div>
-    <div class="LoremIpsumDolorSitAmetConsecteturAdipiscingElitSuspendisseAliquamMolestieCongueSuspendisseVulputateNibhAcEleifendSemper" style="width: 266px; left: 720px; top: 463px; position: absolute; color: white; font-size: 15px;  font-weight: 500; word-wrap: break-word">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam molestie congue. Suspendisse vulputate nibh ac eleifend semper.</div>
-    <div class="Rectangle19" style="width: 90px; height: 40px; left: 1054px; top: 188px; position: absolute; background: black"></div>
-    <div class="Profil" style="left: 1072px; top: 199px; position: absolute; color: white; font-size: 15px;  font-weight: 700; word-wrap: break-word">PROFIL</div>
-    <div class="LetnanJenderalTniHPrabowoSubiantoDjojohadikusumoAdalahSeorangPolitisiPengusahaDanPerwiraTinggiMiliterIndonesiaIaMenempuhPendidikanDanJenjangKarierMiliterSelama28TahunSebelumBerkecimpungDalamDuniaBisnisPolitikDanPemerintahan" style="left: 1054px; top: 244px; position: absolute; color: black; font-size: 12px;  font-weight: 500; letter-spacing: 0.60px; word-wrap: break-word">Letnan Jenderal TNI H. Prabowo Subianto Djojohadikusumo adalah seorang politisi, pengusaha, dan perwira tinggi militer Indonesia. Ia menempuh pendidikan dan jenjang karier militer selama 28 tahun sebelum berkecimpung dalam dunia bisnis, politik dan pemerintahan.</div>
-    <div class="Rectangle20" style="margin-top: 1rem; width: 90px; height: 40px; left: 1054px; top: 363px; position: absolute; background: black"></div>
-    <div class="Agenda" style="margin-top: 0.5remleft: 1066px; top: 374px; position: absolute; color: white; font-size: 15px;  font-weight: 700; word-wrap: break-word">AGENDA</div>
-    <div class="Frame10Variant3" style="width: 1093px; height: 40px; left: 352px; top: 67px; position: absolute; justify-content: flex-end; align-items: center; display: inline-flex">
-      <div class="Frame9" style="justify-content: flex-start; align-items: center; gap: 9px; display: inline-flex">
-        <div class="LoremIpsumDolorSitAmetReynandaAnakKesayanganPapaBin" style="color: white; font-size: 20px;  font-weight: 500; word-wrap: break-word">Lorem Ipsum Dolor Sit Amet, Reynanda anak kesayangan Papa Bin</div>
-        <div class="LoremIpsumDolorSitAmetReynandaAnakKesayanganPapaBin" style="color: white; font-size: 20px;  font-weight: 500; word-wrap: break-word">Lorem Ipsum Dolor Sit Amet, Reynanda anak kesayangan Papa Bin</div>
-        <div class="LoremIpsumDolorSitAmetReynandaAnakKesayanganPapaBin" style="color: white; font-size: 20px;  font-weight: 500; word-wrap: break-word">Lorem Ipsum Dolor Sit Amet, Reynanda anak kesayangan Papa Bin</div>
-      </div>
+    <div class="konten-atas">
+        <table>
+            <thead>
+                <tr>
+                    <th class="atas-kiri">
+                        <img src="image/menhan.png" alt="">
+                    </th>
+                    <th>
+                        <div class="jajar">
+                            <h1 class="no-skew">Untuk Indonesia yang Lebih Baik</h1>
+                            <p class="no-skew2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iusto soluta,
+                                fugit, ad et corporis cumque alias beatae perspiciatis asperiores blanditiis. Suscipit
+                                architecto est a magnam esse, deserunt nihil tempora?</p>
+                            <button class="dukungan">Dukungan</button>
+                            <button class="relawan">Relawan</button>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+        </table>
     </div>
-  </div>
+    <div class="konten-tengah">
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        <button class="info">
+                            <h2>SEKILAS INFO</h2>
+                        </button>
+                    </th>
+                    <th>
+                        <div class="moving-text-container">
+                            <p class="moving-text">Lorem ipsum dolor sit amet, Reynanda anak kesayangan Papa Bin. Lorem
+                                ipsum dolor sit amet, Reynanda anak kesayangan Papa Bin. Lorem ipsum dolor sit amet,
+                                Reynanda anak kesayangan Papa Bin.</p>
+                        </div>
+                    </th>
 
+                </tr>
+            </thead>
+        </table>
+        <table class="konten-tengah-berita">
+            <thead>
+                <tr>
+                    <th class="berita-1">
+                        <img src="image/berita1.png" alt="">
+                        <button>
+                            <h2>25 Juli 2023</h2>
+                        </button>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate temporibus dolore distinctio
+                            omnis exercitationem animi harum placeat illo accusamus consectetur atque, magni cupiditate
+                            error nulla deserunt, aspernatur quaerat laboriosam quibusdam!</p>
+                    </th>
+                    <th class="berita-1">
+                        <img src="image/berita1.png" alt="">
+                        <button>
+                            <h2>25 Juli 2023</h2>
+                        </button>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate temporibus dolore distinctio
+                            omnis exercitationem animi harum placeat illo accusamus consectetur atque, magni cupiditate
+                            error nulla deserunt, aspernatur quaerat laboriosam quibusdam!</p>
+                    </th>
+                    <th class="berita-1">
+                        <img src="image/berita1.png" alt="">
+                        <button>
+                            <h2>25 Juli 2023</h2>
+                        </button>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate temporibus dolore distinctio
+                            omnis exercitationem animi harum placeat illo accusamus consectetur atque, magni cupiditate
+                            error nulla deserunt, aspernatur quaerat laboriosam quibusdam!</p>
+                    </th>
+                    <th class="profile" style="background-color: #ffcc00;">
+                        <div class="buttprof">
+                            <button>
+                                <h2>PROFILE</h2>
+                            </button>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et qui quis, id non beatae illo
+                                delectus quisquam ducimus unde dolorum reiciendis rerum, voluptatibus ipsam neque sit
+                                maiores similique. Minima, modi.</p>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    <div class="konten-tengah-bawah">
+        <h3>BANTU SAYA DENGAN CARA MEMBERIKAN <br> SUPPORT DUKUNGAN UNTUK PERJUANGAN INI</h3>
+        <table class="dukung">
+            <thead>
+                <tr>
+                    <th>
+                        <h3>JADI RELAWAN</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem est praesentium vero unde
+                            cum sint non voluptatibus eos tenetur iure, facere quidem iusto! Corporis iusto est architecto
+                            ex vel similique.</p>
+                        <button style="background-color: #ffcc00; color: #111111;">
+                            BERGABUNG
+                        </button>
+                    </th>
+                    <th>
+                        <h1>10 Juta Orang</h1>
+                        <h4>Telah Memberikan Dukungan</h4>
+                        <button class="dukungan2">
+                            BERI DUKUNGAN
+                        </button>
+
+                    </th>
+                    <th>
+                        <h3>JADI RELAWAN</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem est praesentium vero unde
+                            cum sint non voluptatibus eos tenetur iure, facere quidem iusto! Corporis iusto est architecto
+                            ex vel similique.</p>
+                        <button style="background-color: #ffcc00; color: #111111;">
+                            BERGABUNG
+                        </button>
+                    </th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 @endsection
